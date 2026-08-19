ba9438233e69dab81857deaa3e49e14fe2944e42
Zuvio Global School Admin <info@zuvioglobalschool.com>
2026-08-19 13:02:36 +0530

    debug: simplify verify_status.php

--- verify_status.php
+++ verify_status.php
@@ -1,29 +1,4 @@
 <?php
 header('Content-Type: application/json');
-
-$response = ['step' => 0];
-
-try {
-    $response['step'] = 1;
-    $path = '/home/u869064717/config.php';
-    $response['path'] = $path;
-    
-    $response['step'] = 2;
-    $exists = @file_exists($path);
-    $response['exists'] = $exists;
-    
-    $response['step'] = 3;
-    if ($exists) {
-        $readable = @is_readable($path);
-        $response['readable'] = $readable;
-    }
-    
-    $response['step'] = 4;
-} catch (Throwable $t) {
-    $response['error'] = $t->getMessage();
-    $response['error_file'] = $t->getFile();
-    $response['error_line'] = $t->getLine();
-}
-
-echo json_encode($response);
+echo json_encode(['status' => 'ok']);
 exit;
