c0384fdc565189c60addde899ae281d608cb247b
Zuvio Global School Admin <info@zuvioglobalschool.com>
2026-08-19 12:55:58 +0530

    debug: print config in parse_config.php

--- parse_config.php
+++ parse_config.php
@@ -1,32 +1,17 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: text/plain');
 
 $path = '/home/u869064717/config.php';
 
 if (!file_exists($path)) {
-    echo json_encode(['error' => 'File does not exist']);
+    echo "File not found";
     exit;
 }
 
 $content = file_get_contents($path);
 
-// Match define('CONSTANT', 'value') or define("CONSTANT", "value")
-preg_match_all('/define\s*\(\s*[\'"]([^\'"]+)[\'"]\s*,\s*[\'"]([^\'"]*)[\'"]\s*\)/i', $content, $matches);
+// Obscure DB_PASS using regex replacement
+$obscured = preg_replace("/(define\s*\(\s*['\"]DB_PASS['\"]\s*,\s*['\"])[^'\"]*(['\"]\s*\))/i", "$1******$2", $content);
 
-$definitions = [];
-for ($i = 0; $i < count($matches[0]); $i++) {
-    $key = $matches[1][$i];
-    $val = $matches[2][$i];
-    // Mask password
-    if (strpos($key, 'PASS') !== false || strpos($key, 'SEC') !== false || strpos($key, 'KEY') !== false) {
-        $val = '******';
-    }
-    $definitions[$key] = $val;
-}
-
-echo json_encode([
-    'file_path' => $path,
-    'file_size' => strlen($content),
-    'definitions' => $definitions
-], JSON_PRETTY_PRINT);
+echo $obscured;
 exit;
